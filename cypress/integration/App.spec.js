85ac361ae386a8eda4fe2eb18b7adb9c64475167
Naeem <naeempatel2019@u.northwestern.edu>
2020-06-10 06:17:58 -0500

    new test

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -3,4 +3,15 @@ describe ('Test App', () => {
   it ('launches', () => {
     cy.visit ('/');
   });
+
+  it('opens up email sign in', () => {
+    cy.visit ('/');
+    cy.contains('Sign in with email')
+  });
+
+  it('Google sign in', () => {
+    cy.visit ('/restaurant');
+    cy.get('[data-cy=cuisines]').should('contain', 'American');
+  });
+
 });
